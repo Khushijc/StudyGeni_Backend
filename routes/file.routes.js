ebc655bfe171359cd9ff65bcc9e3d181d537409b
Khushijc <151053815+Khushijc@users.noreply.github.com>
2025-11-13 14:39:08 +0530

    Remove protect middleware from getAllFiles route

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -7,9 +7,10 @@ import { getContentQuiz, getContentSummary } from "../controller/ai.controller.j
 const router = express.Router();
 
 router.post("/", protect, isTeacher, upload.single("file"), uploadFile);
-router.get("/", protect, getAllFiles);
+router.get("/", getAllFiles);
 router.get("/:id", protect, getFileById);
 router.get("/:id/summary",getContentSummary)
 router.get("/:id/quiz",getContentQuiz)
+
 
 export default router;
